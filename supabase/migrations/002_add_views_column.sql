4e16ff889c030e8db3dd5b1a74ebdebd5afd4e0b
MevanManilka <w.a.mevanmanilka@gmail.com>
2026-02-06 17:46:58 +0530

    chore: Remove migration SQL files - no longer needed

--- supabase/migrations/002_add_views_column.sql
+++
@@ -1,5 +0,0 @@
--- Add views column to bugs table
-ALTER TABLE bugs ADD COLUMN IF NOT EXISTS views INTEGER DEFAULT 0;
-
--- Add views column to solutions table
-ALTER TABLE solutions ADD COLUMN IF NOT EXISTS views INTEGER DEFAULT 0;
